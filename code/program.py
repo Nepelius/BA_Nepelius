1c8f4ed14fcddda910536639b686afcd641a51e1
Nepi <nepeliuslukas@gmail.com>
2021-07-21 15:44:58 +0200

    Advanced code for background subtraction (green rectangles)

--- code/program.py
+++ code/program.py
@@ -1,7 +1,8 @@
 import cv2 as cv
 
-backSub = cv.createBackgroundSubtractorMOG2()
+backSub = cv.createBackgroundSubtractorMOG2(history=100, varThreshold=40)
 
+#inVideo = "vtest.avi"
 inVideo = "../data/Schafe.wmv"
 
 capture = cv.VideoCapture(inVideo)
@@ -15,18 +16,34 @@ while True:
         break
 
     # gauß filter
-    gauss = cv.GaussianBlur(frame, (7,7), 0)
+    # gauss = cv.GaussianBlur(frame, (5,5), 0)
 
     # median filter
-    medianFilter = cv.medianBlur(gauss, 7)
-    fgMask = backSub.apply(medianFilter)
+    # medianFilter = cv.medianBlur(gauss, 5)
+    fgMask = backSub.apply(frame)
+    _, fgMask = cv.threshold(fgMask, 254, 255, cv.THRESH_BINARY)
 
-    cv.rectangle(medianFilter, (10, 2), (100, 20), (255, 255, 255), -1)
-    cv.putText(medianFilter, str(capture.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
+    contours, _ = cv.findContours(fgMask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        # get rid of small contour areas
+        area = cv.contourArea(cnt)
+        detections = []
+        if area > 200:
+            # draw contours on original video
+            # cv.drawContours(frame, [cnt], -1, (0, 255, 0), 2)
+
+            # green rectangles
+            x,y,w,h = cv.boundingRect(cnt)
+            cv.rectangle(frame, (x,y), (x + w, y + h), (0, 255, 0), 2)
+
+            cv.imshow('Frame', frame)
+            cv.imshow('FG Mask', fgMask)
+
+    cv.rectangle(fgMask, (10, 2), (100, 20), (255, 255, 255), -1)
+    cv.putText(fgMask, str(capture.get(cv.CAP_PROP_POS_FRAMES)), (15, 15),
                cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
 
-    #cv.imshow('Frame', frame)
-    cv.imshow('FG Mask', fgMask)
+
 
     keyboard = cv.waitKey(30)
     if keyboard == 'q' or keyboard == 27:
